b8bc73172cfc010c751d8b5e61a09d8bc3d4b1c4
Simon Laroche <hi@simon.lc>
2013-09-23 13:22:41 -0400

    More code clean up.

--- tetris.js
+++ tetris.js
@@ -1,13 +1,12 @@
 /*
 Author: Simon Laroche
 Site: http://simon.lc/
+Demo: http://simon.lc/tetr.js
 
 Note: Before looking at this code, it would be wise to do a bit of reading about
 the game so you know why some things are done a certain way.
 */
 'use strict';
-
-var version = '0.1.7';
 
 /**
  * Playfield.
@@ -212,7 +211,6 @@ var setting = {
 
 var frame;
 var inc;
-var setLoop;
 
 /**
  * 0 = Normal
@@ -243,8 +241,6 @@ var keysDown;
 var lastKeys;
 var released;
 var shiftReleased;
-var arrowReleased = true;
-var arrowDelay = 0;
 
 var binds = {
   pause: 27,
@@ -268,33 +264,6 @@ var flags = {
   rotLeft: 64,
   rot180: 128,
 };
-
-var key = {
-  8: 'Backspace', 9: 'Tab',     13: 'Enter',  16: 'Shift',
-  17: 'Ctrl',     18: 'Alt',    19: 'Pause',  20: 'Caps Lock',
-  27: 'Esc',      32: 'Space',  33: 'PgUp',   34: 'PgDn',
-  35: 'End',      36: 'Home',   37: '←',      38: '↑',
-  39: '→',        40: '↓',      45: 'Insert', 46: 'Delete',
-  48: '0',        49: '1',      50: '2',      51: '3',
-  52: '4',        53: '5',      54: '6',      55: '7',
-  56: '8',        57: '9',      59: ';',      61: '=',
-  65: 'A',        66: 'B',      67: 'C',      68: 'D',
-  69: 'E',        70: 'F',      71: 'G',      72: 'H',
-  73: 'I',        74: 'J',      75: 'K',      76: 'L',
-  77: 'M',        78: 'N',      79: 'O',      80: 'P',
-  81: 'Q',        82: 'R',      83: 'S',      84: 'T',
-  85: 'U',        86: 'V',      87: 'W',      88: 'X',
-  89: 'Y',        90: 'Z',      96: '0kpad',  97: '1kpad',
-  98: '2kpad',    99: '3kpad',  100: '4kpad', 101: '5kpad',
-  102: '6kpad',   103: '7kpad', 104: '8kpad', 105: '9kpad',
-  106: '*',       107: '+',     109: '-',     110: '.',
-  111: '/',       112: 'F1',    113: 'F2',    114: 'F3',
-  115: 'F4',      116: 'F5',    117: 'F6',    118: 'F7',
-  119: 'F8',      120: 'F9',    121: 'F10',   122: 'F11',
-  123: 'F12',     173: '-',     187: '=',     188: ',',
-  190: '.',       191: '/',     192: '`',     219: '[',
-  220: '\\',      221: ']',     222: "'"
-}
 
 function resize() {
   var a = document.getElementById('a');
@@ -434,6 +403,7 @@ function init(gt) {
 
   menu();
 
+  // Only start a loop if one is not running already.
   if (gameState === 3) {
     gameState = 2;
     gameLoop();
@@ -482,13 +452,6 @@ function newGrid(x, y) {
   return cells;
 }
 
-function unpause() {
-  paused = false;
-  pauseTime += (Date.now() - startPauseTime);
-  msg.innerHTML = '';
-  menu();
-}
-
 function pause() {
   if (gameState === 0) {
     paused = true;
@@ -496,6 +459,13 @@ function pause() {
     msg.innerHTML = "Paused";
     menu(4);    
   }
+}
+
+function unpause() {
+  paused = false;
+  pauseTime += (Date.now() - startPauseTime);
+  msg.innerHTML = '';
+  menu();
 }
 
 /**
@@ -590,14 +560,6 @@ function addPiece(tetro) {
   drawStack();
 }
 
-function replay(command) {
-  if (command !== void 0) {
-    frame = replayKeys[framePos + command];
-  } else {
-    // toggle play pause and change innerhtml
-  }
-}
-
 function FallingPiece() {
   this.x;
   this.y;
@@ -610,7 +572,6 @@ function FallingPiece() {
   this.arrDelay = 0;
   this.active = false;
   this.held = false;
-
 }
 FallingPiece.prototype.reset = function() {
   this.pos = 0;
@@ -622,7 +583,7 @@ FallingPiece.prototype.reset = function() {
 FallingPiece.prototype.newPiece = function() {
   if (!this.active) {
 
-    // TODO Do this better.
+    // TODO Do this better. Make clone object func maybe.
     //for property in pieces, this.prop = piece.prop
     this.tetro = pieces[grabBag[inc]].tetro;
     this.kickData = pieces[grabBag[inc]].kickData;
@@ -1079,6 +1040,7 @@ function drawStack() {
   clear(stackCtx);
   draw(stack, 0, 0, stackCtx);
 
+  // Darken Stack
   // TODO wrap this with an option.
   stackCtx.globalCompositeOperation = 'source-atop';
   stackCtx.fillStyle = 'rgba(0,0,0,0.3)';
@@ -1164,10 +1126,10 @@ function drawStack() {
 
 // ========================== Controller ======================================
 
-//document.onkeydown = function(e) {
 addEventListener('keydown', function(e) {
   if ([32,37,38,39,40].indexOf(e.keyCode) !== -1)
     e.preventDefault();
+  //TODO if active, prevent default for binded keys
   //if (bindsArr.indexOf(e.keyCode) !== -1)
   //  e.preventDefault();
   if (e.keyCode === binds.pause) {
@@ -1333,7 +1295,8 @@ function gameLoop() {
     /**
      * Fade to grey animation played when player loses.
      */
-    clear(activeCtx); // TODO Move this. (why?)
+    if (toGreyRow === 21)
+      clear(activeCtx);
     if (frame % 2) {
       for (var x = 0; x < 10; x++) {
         if (stack[x][toGreyRow]) stack[x][toGreyRow] = gameState - 1;
